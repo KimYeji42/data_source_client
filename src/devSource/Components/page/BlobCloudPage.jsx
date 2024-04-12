91712e6e2b91606d15dea1373b58cdeb196a8bb1
JOODON <launcher37@naver.com>
2024-04-12 16:49:48 +0900

    이미지 클라우드 부분 정리완료!

--- src/devSource/Components/page/BlobCloudPage.jsx
+++ src/devSource/Components/page/BlobCloudPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import TitleUI from "../../../project/components/uI/TitleUI";
 import styles from "../../styleModule/BlobCloud.module.css";
 import stylesRest from "../../styleModule/restAPIBuilder.module.css";
@@ -8,6 +8,25 @@ import BlobCloudImageLayout from "../layout/BlobCloudImageLayout";
 export default function BlobCloudPage() {
     // 이미지 목록을 관리하는 상태
     const [images, setImages] = useState([]);
+    const [profileID , setProfileID] = useState(1)
+    const fetchCloudAllData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/blob/files/${profileID}`);
+            if (!response.ok) {
+                throw new Error("서버에서 데이터를 가져오는 데 실패했습니다.");
+            }
+            const data = await response.json();
+            setImages(data)
+            return data;
+        } catch (error) {
+            console.error("데이터를 불러오는 중에 오류가 발생했습니다:", error);
+            throw error;
+        }
+    };
+
+    useEffect(() => {
+        fetchCloudAllData()
+    }, []);
 
     // 파일을 업로드하는 함수
     const fileUpload = (event) => {
@@ -16,7 +35,7 @@ export default function BlobCloudPage() {
 
         // 선택된 파일을 FormData에 추가
         formData.append("file", file); // "file"은 서버에서 파일을 처리하는 데 사용될 키 이름
-
+        formData.append("profileID",profileID)
         // 서버로 FormData 전송
         fetch("http://localhost:8080/api/blob/upload", {
             method: "POST",
@@ -25,7 +44,7 @@ export default function BlobCloudPage() {
             .then((response) => response.json())
             .then((data) => {
                 console.log("파일 업로드 성공:", data);
-                setImages(data); //업로드 성공시 반환해준 이미지 업로드
+                setImages((prevImages) => [...prevImages, data]); // 이전 데이터를 유지한 채로 새로운 데이터 추가
             })
             .catch((error) => {
                 console.error("파일 업로드 실패:", error);
