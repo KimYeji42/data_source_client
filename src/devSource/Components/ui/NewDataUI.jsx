593e262bcfaa92cf2b3bd15aacdfb3a5a11d9835
JOODON <launcher37@naver.com>
2024-04-16 23:22:39 +0900

    값 안바뀌는 부분 수정 완료!

--- src/devSource/Components/ui/NewDataUI.jsx
+++ src/devSource/Components/ui/NewDataUI.jsx
@@ -21,15 +21,23 @@ export default function NewDataUI({ column, createData, setCreateData, newDataCo
         setSelectedJoinValue(item); // 선택한 값을 상태로 저장
         const updatedValues = [...newDataValues];
         updatedValues.forEach((value, index) => {
-            updatedValues[index] = item.id || value;
+            updatedValues[index] = item.id || value; // 선택한 값이 있으면 선택한 값의 'id'를 사용하고, 없으면 기존 값 유지
         });
-        setNewDataValues(updatedValues);//이 값이 들어가고 데이터가 닫혔으면 좋게씀..
+
+        setNewDataValues(updatedValues); // 이 값이 들어가고 데이터가 닫혔으면 좋게씀..
         setIsJoinTableMapperModal(false);
+
+        // 선택한 값이 변경될 때마다 입력 필드의 값을 업데이트
+        updatedValues.forEach((value, index) => {
+            handleInputBlur(null, index, value);
+        });
+
     };
+
+
 
     const handleInputBlur = (event, index, value) => {
         const updatedCreateData = [...createData];
-
         const existingIndex = updatedCreateData.findIndex(item => item.columnLine === dataLine && item.columnName === column);
 
         if (existingIndex !== -1) {
@@ -42,6 +50,7 @@ export default function NewDataUI({ column, createData, setCreateData, newDataCo
             columnLine: dataLine
         };
         updatedCreateData.push(obj);
+
         setCreateData(updatedCreateData);
     };
 
@@ -57,10 +66,12 @@ export default function NewDataUI({ column, createData, setCreateData, newDataCo
                             value={value}
                             placeholder="NULL"
                             onBlur={(event) => handleInputBlur(event, index, value)}
-                            onChange={(event) => handleNewDataInputChange(event, index)}
+                            onChange={(event) => {handleNewDataInputChange(event, index);}}
                             onFocus={handleJoinTableChange}
                             readOnly={true}
                         />
+
+
                         {isJoinTableMapperModal &&
                             <JoinTableModalUI
                                 tableID={tableID}
