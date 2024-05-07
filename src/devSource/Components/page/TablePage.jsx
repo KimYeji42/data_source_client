24852a5ad5b7fae891daf62f8b80d1b65fdc1fcc
JOODON <launcher37@naver.com>
2024-05-07 16:41:05 +0900

    템플릿 다운로드 완료 설명추가_폰트 안먹는거 확인하기

--- src/devSource/Components/page/TablePage.jsx
+++ src/devSource/Components/page/TablePage.jsx
@@ -2,17 +2,39 @@ import TableLayout from "../layout/TableLayout";
 import TableTitleUI from "../ui/TableTitleUI";
 import styles from '../../styleModule/ColumnStyle.module.css';
 import DownloadUI from "../ui/DownloadUI";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LinkUI from "../../../project/components/uI/LinkUI";
 import {useParams} from "react-router-dom";
 
 export default function TablePage() {
     const {tableID} = useParams()
+    const [tableInfo , setTableInfo] = useState(null)
+    const findTableInfo = async () => {
+        const apiUrl = process.env.REACT_APP_API_URL;
+
+        try {
+            const response = await fetch(`${apiUrl}/api/database/dataBaseInfo/${tableID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const responseData = await response.json();
+            console.log(responseData)
+            setTableInfo(responseData)
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    useEffect(() => {
+        findTableInfo()
+    }, []);
     return(
         <div>
             <div className={styles.tablePage}>
                 <div className={styles.tableContainer}>
-                    <TableTitleUI title={"[ 프로젝트명 ]"} subTitle={"-[테이블명] Table"}/>
+                    {tableInfo && <TableTitleUI title={"[ " + tableInfo.projectName + " ]"} subTitle={"-" +tableInfo.tableName}/>}
                </div>
                 <div className={styles.linkContainer}>
                     <LinkUI text={"REST API 활용하기"} redirect={`/apiBuilder/${tableID}`}/>
