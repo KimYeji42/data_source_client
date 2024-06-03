757da580d58316039ca29cc7c0b0a7a5267b361a
JOODON <launcher37@naver.com>
2024-06-03 15:03:23 +0900

    마이페이지 데이터 가지고오기

--- src/MyPage/components/page/MyPage.jsx
+++ src/MyPage/components/page/MyPage.jsx
@@ -1,14 +1,59 @@
 import styles from "../../styleModule/MypageStyle.module.css";
 import Profilelayout from "../layout/Profilelayout";
 import Contentslayout from "../layout/Contentslayout";
+import {useEffect, useState} from "react";
+import ErrorModal from "../../../project/components/layout/ErrorModalLayOut";
 
 export default function MyPage(){
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isErrorModalOpen, setErrorModalOpen] = useState(false);
+
+    const fetchProjectData = async (token,endpoint) => {
+        const apiUrl = process.env.REACT_APP_API_URL;
+        try {
+            const response = await fetch(`${apiUrl}${endpoint}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}` // Token을 Header에 포함
+                }
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                const dataArray = Array.isArray(responseData) ? responseData : [responseData];
+                setData(dataArray);
+                console.log(dataArray);
+            } else {
+                throw new Error('마이페이지 데이터를 가져오는데 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('마이페이지 가져오는 중 에러 발생:', error);
+            setError('마이페이지 데이터를 가져오는데 실패했습니다.');
+            setErrorModalOpen(true);
+        }
+    };
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const endpoint = '/api/auth/collections';
+        if (token !== null) {
+            fetchProjectData(token,endpoint);
+            return;
+        }
+        setError("로그인 후 진행해주세요!");
+        setErrorModalOpen(true);
+    }, []);
     return(
-        <div >
+        <div>
             <div className={styles.pageContainer}>
                 <Profilelayout/>
                 <Contentslayout/>
             </div>
+            <ErrorModal
+                error={error}
+                onClose={() => setErrorModalOpen(false)}
+                isOpen={isErrorModalOpen}
+            />
         </div>
     )
 }
