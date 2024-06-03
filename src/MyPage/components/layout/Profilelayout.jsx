af968a980c6512cae78fbeb2b6b3b401357c77dc
BoYoung00 <qhdud20@naver.com>
2024-06-03 16:21:40 +0900

    24/06/03 마이 페이지 완료

--- src/MyPage/components/layout/Profilelayout.jsx
+++ src/MyPage/components/layout/Profilelayout.jsx
@@ -3,14 +3,17 @@ import ProfilePhoto from "../ui/ProfilePhoto";
 import UserName from "../ui/UserName";
 import UserInformation from "../ui/UserInformation";
 
-export default function ProfileLayout(){
+export default function ProfileLayout({ profileInfo }){
+
+    if (!profileInfo) return null;
+
     return(
         <div className={styles.profileContainer}>
             <ProfilePhoto/>
-            <UserName Name="주동호"/>
-            <UserInformation title="이름" value="주동호"/>
-            <UserInformation title="이메일" value="abc@naver.com"/>
-            <UserInformation title="전화번호" value="010-1234-1234"/>
+            <UserName Name={profileInfo.name}/>
+            <UserInformation title="이름" value={profileInfo.name}/>
+            <UserInformation title="이메일" value={profileInfo.email}/>
+            <UserInformation title="전화번호" value={profileInfo.phoneNumber}/>
         </div>
     )
 }
