0c50ecb62ffd47d6c1617baed706cc43b0fc8655
BoYoung00 <qhdud20@naver.com>
2024-06-12 19:13:00 +0900

    알림 추가

--- src/devSource/Components/page/TablePage.jsx
+++ src/devSource/Components/page/TablePage.jsx
@@ -5,17 +5,20 @@ import DownloadUI from "../ui/DownloadUI";
 import React, { useEffect, useState } from "react";
 import LinkUI from "../../../project/components/uI/LinkUI";
 import {Link, useParams} from "react-router-dom";
-import stylesRest from "../../styleModule/restAPIBuilder.module.css";
 import HeaderBottom from "../../../Layout/HeaderBottom/HeaderBottom";
+import SockJS from 'sockjs-client';
+import {Stomp} from "@stomp/stompjs";
 
 export default function TablePage() {
     const { dataBaseID, tableID } = useParams()
     const [tableInfo, setTableInfo] = useState(null)
     const [isOpen, setIsOpen] = useState(false); // 드롭다운 메뉴의 상태
+    const [messages, setMessages] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [stompClient, setStompClient] = useState(null);
+    const apiUrl = process.env.REACT_APP_API_URL;
 
     const findTableInfo = async () => {
-        const apiUrl = process.env.REACT_APP_API_URL;
-
         try {
             const response = await fetch(`${apiUrl}/api/database/dataBaseInfo/${tableID}`, {
                 method: 'GET',
@@ -32,8 +35,28 @@ export default function TablePage() {
         }
     };
 
+
+
     useEffect(() => {
         findTableInfo()
+
+        const socket = new SockJS(`${apiUrl}/websocket-endpoint`);
+        const client = Stomp.over(socket);
+
+        client.connect({}, () => {
+            client.subscribe('/topic/messages', (message) => {
+                const receivedMessage = JSON.parse(message.body);
+                setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+            });
+
+            setStompClient(client);
+        });
+
+        return () => {
+            if (stompClient) {
+                stompClient.disconnect();
+            }
+        };
     }, []);
 
     // 드롭다운 토글 함수
@@ -44,6 +67,9 @@ export default function TablePage() {
     return (
         <>
             <HeaderBottom title={"테이블"} titleList={["프로젝트 목록", "프로젝트", "데이터베이스"]} linkList={["/projects", `/project/${dataBaseID}`, `/tables/${dataBaseID}`]}/>
+            {messages.map((message, index) => (
+                <li key={index}>{message}</li>
+            ))}
             <div className={styles.tablePage}>
                 <div className={styles.tableContainer}>
                     {tableInfo && <TableTitleUI title={"[ " + tableInfo.projectName + " ]"} subTitle={"- " + tableInfo.tableName} />}
